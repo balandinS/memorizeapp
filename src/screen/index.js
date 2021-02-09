53d3fb27120f724c5f1dc1e5038deae25c5e9795
serg <seregey.balandin@gmail.com>
2021-02-09 18:01:09 +0200

    Login flow
    1. add condation if user login or not
    2.dont render login screen if user ligin once

---
+++ src/screen/index.js
@@ -0,0 +1,11 @@
+import HomeScreen from './HomeScreen';
+import SignupScreen from './SignupScreen';
+import SinginScreen from './SinginScreen';
+import WelcomeScreen from './WelcomeScreen';
+
+export {
+    HomeScreen,
+    SignupScreen,
+    SinginScreen,
+    WelcomeScreen
+}
