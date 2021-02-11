9f47c7f2bf01f792a15f631a211f0c65d4846d1b
serg <seregey.balandin@gmail.com>
2021-02-11 16:39:26 +0200

    test for analytics

--- src/screen/SinginScreen/SigninScreen.js
+++ src/screen/SinginScreen/SigninScreen.js
@@ -1,14 +1,27 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-
+import React from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import analytics from '@react-native-firebase/analytics';
 const SigninScreen = () => {
-    return (
-        <View>
-            <Text>SigninScreen</Text>
-        </View>
-    )
-}
+  return (
+    <View>
+      <Text>SigninScreen</Text>
+      <View>
+        <Button
+          title="Add To Basket"
+          onPress={async () =>
+            await analytics().logEvent('basket', {
+              id: 3745092,
+              item: 'mens grey t-shirt',
+              description: ['round neck', 'long sleeved'],
+              size: 'L',
+            })
+          }
+        />
+      </View>
+    </View>
+  );
+};
 
-export default SigninScreen
+export default SigninScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
