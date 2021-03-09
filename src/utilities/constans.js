b614e051b5e89b38680f529da05960890d50cc09
serg <seregey.balandin@gmail.com>
2021-03-09 14:32:23 +0200

    global var

---
+++ src/utilities/constans.js
@@ -0,0 +1,7 @@
+import { exp } from "react-native/Libraries/Animated/src/Easing";
+
+export const COLORS = {
+   peterriver:  'rgba(52, 152, 219,1.0)',
+   white: 'white',
+   clouds: 'rgba(189, 195, 199,1.0)' 
+}
