838922903702af5436f8901b69c0c792ee1a9c91
serg <seregey.balandin@gmail.com>
2021-03-18 14:47:04 +0200

    add more global data

--- src/utilities/constans.js
+++ src/utilities/constans.js
@@ -3,5 +3,23 @@ import { exp } from "react-native/Libraries/Animated/src/Easing";
 export const COLORS = {
    peterriver:  'rgba(52, 152, 219,1.0)',
    white: 'white',
-   clouds: 'rgba(189, 195, 199,1.0)' 
+   clouds: 'rgba(189, 195, 199,1.0)',
+   black: 'rgb(17 ,10, 8)',
+   white: 'rgb(255,255,255)',
+   beige: 'rgb(229, 222, 216)',
+   gray: 'rgb(247,247,247)',
+   blackLight: 'rgb(37, 36, 42)',
+   blue: 'rgb(64, 150, 237)',
+   red: 'rgb(235, 51, 36)',
+   graydark: 'rgb(174, 174, 174)'
+}
+
+export const SIGNIN_SCREEN_CONTENT = {
+   title: 'Log into \nyour account',
+   description: 'Or sign in with socail account'
+}
+
+export const SIGNUP_SCREEN_CONTENT = {
+   title: 'create \nyour account',
+   description: 'Or sign in with socail account'
 }
