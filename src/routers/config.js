c6fbda78febb634f5b7426df62f592a7a8f9df63
serg <seregey.balandin@gmail.com>
2021-03-18 14:44:49 +0200

    add config to signup screen navigation header

--- src/routers/config.js
+++ src/routers/config.js
@@ -1,19 +1,47 @@
-import { TransitionSpecs, HeaderStyleInterpolators } from '@react-navigation/stack';
+import React from 'react';
+import {TouchableOpacity, StyleSheet} from 'react-native';
+import {
+  TransitionSpecs,
+  HeaderStyleInterpolators,
+} from '@react-navigation/stack';
+import {COLORS} from '../utilities/constans';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+import {goBack} from './rootNavigation';
 
 export const FadeInOutAnimation = {
-    transitionSpec: {
-        open: TransitionSpecs.TransitionIOSSpec,
-        close: TransitionSpecs.TransitionIOSSpec,
-    },
-    headerStyleInterpolator: HeaderStyleInterpolators.forFade,
-    cardStyleInterpolator: ({ current, next, layouts }) => {
-        return {
-            cardStyle: {
-                opacity: current.progress.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [0, 1],
-                }),
-            },
-        };
-    },
+  transitionSpec: {
+    open: TransitionSpecs.TransitionIOSSpec,
+    close: TransitionSpecs.TransitionIOSSpec,
+  },
+  headerStyleInterpolator: HeaderStyleInterpolators.forFade,
+  cardStyleInterpolator: ({current, next, layouts}) => {
+    return {
+      cardStyle: {
+        opacity: current.progress.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, 1],
+        }),
+      },
+    };
+  },
 };
+
+export const signupOptionNavigate = {
+  headerStyle: {
+    backgroundColor: COLORS.beige,
+    elevation: 0
+  },
+  headerTitle: '',
+  headerLeft: (props) => (
+    <TouchableOpacity style={styles.singup} onPress={goBack}>
+      <Icon name="long-arrow-left" size={30} color={COLORS.blackLight} />
+    </TouchableOpacity>
+  ),
+};
+
+const styles = StyleSheet.create({
+  singup: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+  },
+});
