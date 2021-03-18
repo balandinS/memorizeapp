621d7b4ed3597d0b4206b029b6a29728f2da7185
serg <seregey.balandin@gmail.com>
2021-03-18 14:42:57 +0200

    refactor input componenet

--- src/component/InputText/Input.js
+++ src/component/InputText/Input.js
@@ -1,6 +1,8 @@
 import React, {useRef, useState, useEffect} from 'react';
-import {StyleSheet, Text, View, TextInput, Animated, Easing} from 'react-native';
+import {StyleSheet, View, TextInput, Animated, Easing, TouchableOpacity} from 'react-native';
+import Text from '../Text'
 import {COLORS} from '../../utilities/constans';
+import { SIGNIN } from '../../routers/types'
 const Input = (props) => {
   const [isFocus, setIsFocus] = useState(false);
   const labelAnime = useRef(new Animated.Value(!props.value ? 1 : 0)).current
@@ -19,33 +21,46 @@ const Input = (props) => {
   })
   return (
     <View style={styles.container}>
+      
       <Animated.Text style={[styles.label, {
           transform:[{
             translateY: labelAnime.interpolate({
                 inputRange: [0, 1],
                 outputRange: [0, -25],
-            }),
-           
+              })
             },
-          {
+            {
+              translateX: labelAnime.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, -6],
+                })
+              },
+            {
             scale: labelAnime.interpolate({
               inputRange: [0, 1],
               outputRange: [1, .8],
-          })
+            })
           }],
             zIndex: 1,
         }]}
         onPress={()=> refInput.current.focus()}
         >{props.label}</Animated.Text>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
       <TextInput
         ref={refInput}
         secureTextEntry={props.isPassword}
         onBlur={handleOnBlur}
         onFocus={handleOnFocus}
         value={props.value}
+        onEndEditing={props.onEndEditing}
         style={styles.input}
         onChangeText={(text) => props.onChangeText(text)}
       />
+      <TouchableOpacity style={{alignItems: 'center',justifyContent: 'center' ,width: 70}}>
+         {props.isPassword && props.screenName === SIGNIN && <Text style={styles.forgot}>Forgot?</Text>}
+      </TouchableOpacity>
+      </View>
+      {props.isErrMsg.isValid && <Text style={styles.errorMsg}>*{props.isErrMsg.msg}</Text>}
     </View>
   );
 };
@@ -59,13 +74,22 @@ const styles = StyleSheet.create({
   label: {
     position: 'absolute',
     fontSize: 18,
-    padding: 2,
-    paddingHorizontal: 3,
-
+    left: 0,
+    lineHeight: 20,
     color: 'rgb(145, 145, 145)',
     
   },
   input: {
     height: 35,
+    minWidth: 250
   },
+  errorMsg :{
+     fontSize: 12,
+     color: COLORS.red
+  },
+  forgot :{
+    fontSize: 15,
+    lineHeight: -5,
+    color: COLORS.graydark
+ }
 });
