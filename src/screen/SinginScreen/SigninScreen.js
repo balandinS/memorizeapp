2b765f02e06058a2da42c0d3f620d07698215cbc
serg <seregey.balandin@gmail.com>
2021-03-18 14:46:33 +0200

    refactor signin screen

--- src/screen/SinginScreen/SigninScreen.js
+++ src/screen/SinginScreen/SigninScreen.js
@@ -1,46 +1,117 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, ScrollView} from 'react-native';
 import {Formik} from 'formik';
 import Text from '../../component/Text';
 import Input from '../../component/InputText';
-import {COLORS} from '../../utilities/constans';
-import {values} from 'lodash';
-const SigninScreen = () => {
+import UIButton from '../../component/Button';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+import {COLORS, SIGNIN_SCREEN_CONTENT} from '../../utilities/constans';
+import {signinScheme} from '../../component/InputText/validationSchema';
+import { useNavigation } from '@react-navigation/native';
+import { SIGNUP } from '../../routers/types'
+const SigninScreen = (props) => {
+  const navigation = useNavigation();
+  const handleSignup = () => navigation.navigate(SIGNUP)
   return (
     <View style={styles.container}>
       <View style={styles.containerTitle}>
-        <Text style={styles.title}>Log into</Text>
-        <Text style={styles.title}>your account</Text>
+        <Text style={styles.title}>{SIGNIN_SCREEN_CONTENT.title}</Text>
       </View>
-      <View style={styles.containerForm}>
+      <ScrollView style={styles.containerForm}>
         <Formik
           initialValues={{
             email: '',
             password: '',
             remember: false,
           }}
+          validationSchema={signinScheme}
           onSubmit={(values) => {
             console.log(values);
           }}>
-          {({handleChange, handleBlur, handleSubmit, values}) => (
-            <View style={styles.conatinerInpunts}>
-              <Input
-                label="E-mail"
-                onChangeText={handleChange('email')}
-                value={values.email}
-              />
-              <View style={{height:2, backgroundColor: 'rgb(247,247,247)'}}/>
-              <Input
-                label="Password"
-                isPassword
-                onChangeText={handleChange('password')}
-                value={values.password}
-              />
-            </View>
+          {({
+            values,
+            handleChange,
+            handleSubmit,
+            setFieldTouched,
+            errors,
+            touched,
+            isValid,
+          }) => (
+            <>
+              <View style={styles.conatinerInpunts}>
+                <View style={styles.inputWrapp}>
+                  <Input
+                    label="E-mail"
+                    onEndEditing={() => setFieldTouched('email')}
+                    onChangeText={handleChange('email')}
+                    value={values.email}
+                    isErrMsg={{
+                      isValid: touched.email && errors.email,
+                      msg: errors.email,
+                    }}
+                  />
+                </View>
+
+                <View style={styles.hr} />
+                <View style={styles.inputWrapp}>
+                  <Input
+                    label="Password"
+                    isPassword
+                    screenName = {props.route.name}
+                    onChangeText={handleChange('password')}
+                    onEndEditing={() => setFieldTouched('password')}
+                    value={values.password}
+                    isErrMsg={{
+                      isValid: touched.password && errors.password,
+                      msg: errors.password,
+                    }}
+                  />
+                </View>
+              </View>
+              <View
+                style={{
+                  marginTop: 20,
+                  height: 110,
+                  justifyContent: 'space-around'
+                }}>
+                <UIButton contnet="Log In" />
+                <UIButton onPress={handleSignup}  propStyle={styles.signup} contnet="Sign Up" textColor={COLORS.blackLight}/>
+              </View>
+              <View style={styles.descriptionConatiner}>
+                <Text style={styles.description}>
+                  {SIGNIN_SCREEN_CONTENT.description}
+                </Text>
+                <View style={styles.socailGroup}>
+                  <UIButton
+                    isDisable={!isValid}
+                    onPress={handleSubmit}
+                    propStyle={styles.facebookBtn}
+                    icon={
+                      <Icon
+                        name="facebook"
+                        size={26}
+                        color={COLORS.white}
+                        style={styles.icon}
+                      />
+                    }
+                  />
+                  <UIButton
+                    propStyle={styles.googleBtn}
+                    icon={
+                      <Icon
+                        name="google"
+                        size={26}
+                        color={COLORS.white}
+                        style={styles.icon}
+                      />
+                    }
+                  />
+                </View>
+              </View>
+            </>
           )}
         </Formik>
-      </View>
-      <View style={styles.containerButtom}></View>
+      </ScrollView>
     </View>
   );
 };
@@ -51,24 +122,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    paddingHorizontal: 15,
+    paddingHorizontal: 20,
     paddingVertical: 8,
-    backgroundColor: 'rgb(229, 222, 216)',
+    backgroundColor: COLORS.beige,
   },
   containerTitle: {
-    flex: 1,
+    flex: 0.3,
     justifyContent: 'flex-end',
     paddingHorizontal: 10,
   },
   containerForm: {
-    flex: 2,
+    flex: 3,
   },
   conatinerInpunts: {
     marginTop: 20,
     justifyContent: 'center',
-    backgroundColor: 'rgb(255,255,255)',
+    backgroundColor: COLORS.white,
     borderRadius: 10,
-    flex: .5,
+    flex: 0.35,
+    shadowColor: COLORS.black,
+    shadowOffset: {
+      width: 6,
+      height: 6,
+    },
+    shadowOpacity: 0.3,
+    elevation: 6
   },
   containerButtom: {
     flex: 1,
@@ -76,7 +154,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 31,
     fontWeight: 'bold',
-    color: 'rgb(17 ,10, 8)',
+    color: COLORS.black,
   },
   containerEdit: {
     flex: 0.2,
@@ -88,5 +166,54 @@ const styles = StyleSheet.create({
   remember: {
     color: COLORS.peterriver,
     fontSize: 18,
+  },
+  hr: {
+    height: 2,
+    backgroundColor: COLORS.gray,
+  },
+  inputWrapp: {
+    padding: 15,
+    flex: 1,
+  },
+  descriptionConatiner: {
+    flex: 0.3,
+    justifyContent: 'center',
+  },
+  description: {
+    marginTop: 20,
+    color: COLORS.black,
+    fontSize: 15,
+    textAlign: 'center',
+  },
+  signup: {
+    borderRadius: 30,
+    alignItems: 'center',
+    flex: 0.3,
+    borderWidth: 1,
+    borderColor: COLORS.blackLight,
+  },
+  socailGroup: {
+    marginTop: 20,
+    flex: 0.45,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  facebookBtn: {
+    backgroundColor: COLORS.blue,
+    flex: 0.3,
+    borderRadius: 30,
+    borderWidth: 1,
+    borderColor: COLORS.blue,
+  },
+  googleBtn: {
+    backgroundColor: COLORS.red,
+    borderRadius: 30,
+    alignItems: 'center',
+    flex: 0.3,
+    borderWidth: 1,
+    borderColor: COLORS.red,
+  },
+  icon: {
+    textAlign: 'center',
   },
 });
