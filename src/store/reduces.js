bcdd2fd0079f7b81425223376838b4def442314b
serg <seregey.balandin@gmail.com>
2021-04-02 12:50:53 +0300

    rename and refactor in store user data
    1. action and pass data with login

--- src/store/reduces.js
+++ src/store/reduces.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 import LoginReducer from './login/LoginReducer'
 import UiReducer from './ui/UiReducer'
 import ForogtPasswordReducer from './forgotPassword/ForogtPasswordReducer'
+import UserReducer from './userdata/UserReducer'
 export default combineReducers({
     LoginReducer,
     UiReducer,
-    ForogtPasswordReducer
+    ForogtPasswordReducer,
+    UserReducer
 })
--- src/store/rootSaga.js
+++ src/store/rootSaga.js
@@ -1,7 +1,9 @@
 import { all, fork } from 'redux-saga/effects'
 import forgotPassword from './forgotPassword/ForgotPasswordSaga'
+import userSaga from './userdata/UserSaga'
 export function* rootSaga() {
    yield all([
-       fork(forgotPassword)
+       fork(forgotPassword),
+       fork(userSaga)
    ])
 }
