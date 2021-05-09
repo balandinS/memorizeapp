aa18152e0b171ae4a74eaf59ba55b801bc21b2cf
serg <seregey.balandin@gmail.com>
2021-05-09 16:01:01 +0300

    helper to calc size

--- src/utilities/screenUtilities.js
+++ src/utilities/screenUtilities.js
@@ -1,9 +1,15 @@
 //@flow
 import {Dimensions} from 'react-native';
-const {width, height} = Dimensions.get('window');
+type Window = {
+  width: number,
+  height: number,
+};
+
+const window: Window = Dimensions.get('window');
 const guidelineBaseWidth: number = 350;
 const guidelineBaseHeight: number = 680;
+
 export const scale = (size: number): number =>
-  (width / guidelineBaseWidth) * size;
+  (window.width / guidelineBaseWidth) * size;
 export const verticalScale = (size: number): number =>
-  (height / guidelineBaseHeight) * size;
+  (window.height / guidelineBaseHeight) * size;
