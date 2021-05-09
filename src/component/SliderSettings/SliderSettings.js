5b737b0a7fa454f432c9df6f378360f3748fe57f
serg <seregey.balandin@gmail.com>
2021-05-09 17:55:49 +0300

    new generic component

---
+++ src/component/SliderSettings/SliderSettings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+const SliderSettings = () => {
+  return (
+    <View>
+      <Icon name="sliders" color={COLORS.graydark} size={22} />
+    </View>
+  );
+};
+
+export default SliderSettings;
+
+const styles = StyleSheet.create({});
