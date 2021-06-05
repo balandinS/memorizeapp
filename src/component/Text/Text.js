76cb5890038f81dd1cd4630f1edad52e7becc036
serg <seregey.balandin@gmail.com>
2021-06-05 19:10:07 +0300

    refactor

--- src/component/Text/Text.js
+++ src/component/Text/Text.js
@@ -1,11 +1,7 @@
-import React, {useRef, useEffect} from 'react';
-import {View, Animated, StyleSheet, Easing} from 'react-native';
+import React from 'react';
+import {View, Animated} from 'react-native';
 import {useFadeAnime, useTranslateAnime} from '../../utilities/hooks';
 import PropTypes from 'prop-types';
-const PADDING_VERTICAL = 8;
-const PADDING_HORIZONTAL = 5;
-const PADDING = 5;
-const MARGIN = 10;
 
 export default function Text(props) {
   const {children} = props;
@@ -24,16 +20,4 @@ export default function Text(props) {
 
 Text.propTypes = {
   children: PropTypes.string.isRequired,
-  style: PropTypes.object,
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: PADDING,
-    paddingHorizontal: PADDING_HORIZONTAL,
-    paddingVertical: PADDING_VERTICAL,
-    marginTop: MARGIN,
-    marginBottom: MARGIN,
-  },
-});
